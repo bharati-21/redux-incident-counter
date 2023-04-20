5229ed91f804645be3b07be336c8a39ec0213ef9
Bharati Subramanian <bharatisharada@gmail.com>
2023-04-20 17:02:59 +0530

    feat: dispatch increment, decrement, set

--- src/Counter.js
+++ src/Counter.js
@@ -1,15 +1,26 @@
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment, set } from "./actions";
+
 export const Counter = () => {
   const incident = 'Incident';
-  const count = 0;
+  const count = useSelector(({ count }) => count);
+  const dispatch = useDispatch();
+
+  const handleSet = () => {
+    dispatch(set(0));
+  }
+
+  const handleIncrement = () => dispatch(increment());
+  const handleDecrement = () => dispatch(decrement());
 
   return (
     <main className="Counter">
       <h1>Days Since Last {incident}</h1>
       <p className="count">{count}</p>
       <section className="controls">
-        <button>Increment</button>
-        <button>Reset</button>
-        <button>Decrement</button>
+        <button onClick={handleIncrement}>Increment</button>
+        <button onClick={handleSet}>Reset</button>
+        <button onClick={handleDecrement}>Decrement</button>
       </section>
     </main>
   );
