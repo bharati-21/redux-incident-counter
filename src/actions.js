1f248cd285dfe249438e22e0069603aa258b4c21
Bharati Subramanian <bharatisharada@gmail.com>
2023-04-20 17:01:55 +0530

    refactor: typo

--- src/actions.js
+++ src/actions.js
@@ -4,4 +4,4 @@ export const SET = "SET";
 
 export const increment = () => ({ type: INCREMENT });
 export const decrement = () => ({ type: DECREMENT });
-export const set = (value) => ({ type: SET, paylod: value });
+export const set = (value) => ({ type: SET, payload: value });
